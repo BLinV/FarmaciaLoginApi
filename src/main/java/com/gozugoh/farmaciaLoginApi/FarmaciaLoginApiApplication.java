16a630c4d4fa257b58aeb88096ed5b8756f1de9d
BLinV <linares.98@hotmail.com>
2024-11-06 15:54:55 -0500

    Primer commit, creacion de proyecto.

---
+++ src/main/java/com/gozugoh/farmaciaLoginApi/FarmaciaLoginApiApplication.java
@@ -0,0 +1,13 @@
+package com.gozugoh.farmaciaLoginApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaLoginApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaLoginApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gozugoh/farmaciaLoginApi/FarmaciaLoginApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gozugoh.farmaciaLoginApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaLoginApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
